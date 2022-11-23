0147aca04bacfc4d4c814409dbdb2bd168fd9d46
Eddy <eddyjohn.terosier@hotmail.com>
2022-11-23 16:34:48 +0100

    conditional rendering over

--- Fondamentaux3/conditional-rendering/src/App.js
+++ Fondamentaux3/conditional-rendering/src/App.js
@@ -2,30 +2,30 @@ import logo from './logo.svg';
 import {useState} from "react";
 import './App.css';
 
-function TodayDate(props) {
+function TodayDate({isVisible}) {
   const [date, setDate] = useState(`${new Date()}`);
   const update = () => {
     setDate(`${new Date()}`);
   };
   return(
     <>
-    <p>{props.isVisible && date}</p>
+    <p>{isVisible && date}</p>
       <button onClick={update}>MAJ date</button><br/>
     </>
   )
 }
 
 function App() {
-  const [visible, setVisible] = useState(true);
-  const toggleVisibility = () => setVisible(!visible);
+  const [isVisible, setVisible] = useState(true);
+  const toggleVisibility = () => setVisible(!isVisible);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <TodayDate isVisible={visible}/>
-        {visible &&<button onClick={toggleVisibility}>hide</button>}
-        {!visible &&<button onClick={toggleVisibility}>show</button>}
+        <TodayDate isVisible={isVisible}/>
+        {isVisible &&<button onClick={toggleVisibility}>hide</button>}
+        {!isVisible &&<button onClick={toggleVisibility}>show</button>}
       </header>
     </div>
   );
