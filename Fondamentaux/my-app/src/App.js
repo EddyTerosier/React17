28eb9c4a8a91685b56dc59a141c127831413129c
Eddy <eddyjohn.terosier@hotmail.com>
2022-11-23 11:02:51 +0100

    Creation de composant

--- Fondamentaux/my-app/src/App.js
+++ Fondamentaux/my-app/src/App.js
@@ -1,6 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Hello() {
+  return (
+    <>
+      <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+    </>
+  )
+}
+
 function App() {
   return (
     <div className="App">
@@ -9,14 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Hello/>
       </header>
     </div>
   );
